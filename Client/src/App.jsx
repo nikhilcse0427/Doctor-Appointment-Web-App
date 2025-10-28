f498f8d580f3f4004c28102d3f7e6bf3afa56a25
nikhilcse0427 <nikhilcse0427@gmail.com>
2025-10-29 01:05:38 +0530

    Frontend React  project created, tailwindcss and shadcn configured and tested, and Backend Express projected created by ensuring proper folder and file structure.

---
+++ Client/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { ArrowUpIcon } from "lucide-react"
+import { Button } from "./components/ui/button"
+function App() {
+
+  return (
+    <>
+      <h1 className='text-4xl text-blue-400 font-bold text-center'>Doctor Appointment App</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
